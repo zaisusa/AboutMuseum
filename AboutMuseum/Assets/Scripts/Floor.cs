39787669f98e0b4b452167e9443571dad2dea96f
zaisusa <vinnikovvanya123@gmail.com>
2024-03-17 20:22:12 +0700

    Map and Excursion windows

---
+++ AboutMuseum/Assets/Scripts/Floor.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Floor : MonoBehaviour
+{
+    public bool isOpen = false;
+    ToggleGroup toggles;
+    // Start is called before the first frame update
+    
+}
---
+++ AboutMuseum/Assets/Scripts/Valuation.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Valuation : MonoBehaviour
+{
+    public int Value =5;
+    [SerializeField]GameObject[] stars;
+
+    public Sprite[] ImageStars;
+    void Start()
+    {
+        
+        for (int i=0; i<5; i++)
+        {
+            stars[i] = transform.GetChild(i).gameObject;
+        }
+        SwapColor(1, 0);
+    }
+
+    public void SetValue(int _value)
+    {
+        Value = _value;
+        SwapColor(0,1);
+        
+    }
+    void SwapColor(int goldstars, int whitestars)
+    {
+        if(Value == 5)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                stars[i].GetComponent<RawImage>().texture = ImageStars[goldstars].texture;
+            }
+        }
+        else
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                stars[i].GetComponent<RawImage>().texture = ImageStars[whitestars].texture;
+            }
+            for (int i = 0; i < Value; i++)
+            {
+                stars[i].GetComponent<RawImage>().texture = ImageStars[goldstars].texture;
+            }
+        }
+        
+        
+    }
+}
